a7e2699239ecaa89fb38136aff20d0f8a7099aca
Andrey <spiridonov.new@gmail.com>
2020-08-03 10:57:43 +0300

    add styles to schedule calendar component

--- src/components/ScheduleCalendar/ScheduleCalendar.component.tsx
+++ src/components/ScheduleCalendar/ScheduleCalendar.component.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import styled from 'styled-components/native';
-import {Text} from 'react-native';
 
 interface IScheduleCalendarComponent {
   currDate: Date;
+}
+
+interface IScheduleCalendarDay {
+  label: string;
+  date: Date;
 }
 
 const ScheduleCalendarComponent: React.FC<IScheduleCalendarComponent> = ({
@@ -14,19 +18,29 @@ const ScheduleCalendarComponent: React.FC<IScheduleCalendarComponent> = ({
   const calendarStartDate = new Date(currDate);
   calendarStartDate.setDate(currDate.getDate() - startDayOfWeek + 1);
 
-  const dates = [0, 1, 2, 3, 4, 5, 6].map((_: any, index: number) => {
-    const newDate = new Date(calendarStartDate);
-    newDate.setDate(newDate.getDate() + index);
+  const dates = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].map(
+    (value: string, index: number) => {
+      const newDate = new Date(calendarStartDate);
+      newDate.setDate(newDate.getDate() + index);
 
-    return newDate;
-  });
+      return {
+        label: value,
+        date: newDate,
+      } as IScheduleCalendarDay;
+    },
+  );
 
   return (
     <ScheduleCalendarContainer>
-      {dates.map((item: Date) => (
-        <ScheduleCalendarOption key={item.getTime()}>
-          <Text>{item.getDay()}</Text>
-          <Text>{item.getDate()}</Text>
+      {dates.map((item: IScheduleCalendarDay) => (
+        <ScheduleCalendarOption key={item.date.getTime()}>
+          <ScheduleCalendarOptionLabel>
+            {item.label}
+          </ScheduleCalendarOptionLabel>
+          <ScheduleCalendarOptionDate
+            isSelect={item.date.getTime() === currDate.getTime()}>
+            {item.date.getDate()}
+          </ScheduleCalendarOptionDate>
         </ScheduleCalendarOption>
       ))}
     </ScheduleCalendarContainer>
@@ -38,8 +52,36 @@ const ScheduleCalendarContainer = styled.View`
   flex-direction: row;
 
   justify-content: space-between;
+
+  border-top-width: 1px;
+  border-top-color: ${'#E4E4E4'};
+
+  padding-top: 10px;
 `;
 
-const ScheduleCalendarOption = styled.View``;
+const ScheduleCalendarOption = styled.View`
+  display: flex;
+  align-items: center;
+`;
+
+const ScheduleCalendarOptionLabel = styled.Text`
+  font-size: 14px;
+  font-family: 'Inter-Regular';
+
+  color: ${'#C4C4C4'};
+`;
+
+const ScheduleCalendarOptionDate = styled.Text<{isSelect: boolean}>`
+  font-size: 14px;
+  font-family: 'Inter-Regular';
+
+  margin-top: 2px;
+  padding: 3px 8px;
+
+  border-radius: 5px;
+
+  background-color: ${(props) => (props.isSelect ? '#444444' : '#ffffff')};
+  color: ${(props) => (props.isSelect ? '#ffffff' : '#000000')};
+`;
 
 export default ScheduleCalendarComponent;
