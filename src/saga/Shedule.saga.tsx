c897941ca62ee387a7b6f30526286e5b4987c2a5
Andrey <spiridonov.new@gmail.com>
2020-09-23 22:46:31 +0300

    fix bug with wrong view schedule

--- src/saga/Shedule.saga.tsx
+++ src/saga/Shedule.saga.tsx
@@ -24,7 +24,7 @@ const processSchedule = (data: any): [Map<number, ILesson[]>, number] => {
       const resTimestamp = new Date(
         date.getFullYear(),
         date.getMonth(),
-        date.getDate() - 1, // BUG
+        date.getDate(), // BUG
       ).getTime();
 
       let newLesson = new Lesson(
